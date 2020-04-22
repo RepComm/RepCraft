0eb367ba5e7a13e401dd14f0110506a519606741
RepComm <rccommandotac@gmail.com>
2020-04-21 19:55:22 -0500

    Git ammend sucks ass. Here's the corrected code.

--- src/main/java/com/roguecircuitry/repcraft/resources/repcraft.mjs
+++ src/main/java/com/roguecircuitry/repcraft/resources/repcraft.mjs
@@ -1,43 +1,101 @@
 
-/**Get value from key (global or from passed `obj`)
- * @param obj that contains the key
- * @param key that references the value
- */
-function getFromKey (obj, key) {
-  if (obj) return obj[key];
-  return globalThis[key];
+//POLYFILL because no window object
+let window = globalThis;
+//END POLYFILL
+
+const getProps = (obj) => {
+  let properties = new Set();
+  let currentObj = obj;
+  do {
+    Object.getOwnPropertyNames(currentObj).map(item => properties.add(item));
+  } while ((currentObj = Object.getPrototypeOf(currentObj)))
+  return [...properties.keys()];//.filter(item => typeof obj[item] === 'function')
 }
 
-/**
- * @param {String} toAutoComplete
- */
-function onTabComplete (toAutoComplete) {
-  let results = new Array();
-  let objLinkKeys = toAutoComplete.split(".");
+const keyToDesc = (obj, key) => {
+  return key + " : " + typeof(obj[key]);
+}
 
-  let lastFoundObj = globalThis;
-  let temp;
-  let key;
-  let foundKeys;
+export class EvalComplete {
+  constructor() {
+    this.lastObj = undefined;
+    this.potentialObj = undefined;
+    this.prop = undefined;
+    this.lastKeys = undefined;
 
-  for (let i=0; i<objLinkKeys.length; i++) {
-    key = objLinkKeys[key];
+    this.completeStrings = new Array();
+  }
 
-    temp = getFromKey(lastFoundObj, key);
-    if (temp) {
-      lastFoundObj = temp;
-      //TODO - also search for larger autocompletes if we're at the last key
-      continue;
+  /**Try to complete a string
+   * Values that can complete are stored in completeStrings property
+   * @param {string} str String to auto-complete
+   * @param {boolean} allPropsOfStr true when trying to get all props of object string refers to
+   * @returns {boolean} successful iteration or not (not necessarily fail when false)
+   */
+  complete(str, allPropsOfStr = false) {
+    if (str.includes(" ")) {
+      str = str.split(" ").pop();
     }
-    if (lastFoundObj) {
-      foundKeys = Object.keys(lastFoundObj);
-      for (let foundKey of foundKeys) {
-        if (key === "." || key === "" || foundKey.includes(key)) {
-          results.push(foundKey);
+    if (allPropsOfStr) {
+      str = str.substring(0, str.length - 1);
+      try {
+        //Try to get an object represented by the string first
+        this.potentialObj = eval(str);
+        //If the object isn't falsy
+        if (this.potentialObj) {
+          //Set last object to evaluated object
+          this.lastObj = this.potentialObj;
+          //Set complete strings to all keys of object
+          this.completeStrings = getProps(this.lastObj);
+          this.completeStrings.forEach((v)=>{
+            return keyToDesc(this.lastObj, v);
+          });
+        }
+      } catch (ex) {
+        //No match
+        console.warn(ex);
+      }
+    } else {
+      //if (this.lastObj) {
+      //Get the last bit after the last period
+      this.prop = str.split(".").pop();
+
+      //If we completed the word property return it by itself
+      if (this.lastObj && this.lastObj[this.prop]) {
+        this.completeStrings.length = 1;
+        this.completeStrings[0] = keyToDesc(this.lastObj, this.prop);
+      } else { //Else, try to complete the property be seeing if we have a part of it
+        if (this.lastObj === undefined) {
+          //If we don't have a last object and we're not referencing subobjects
+          if (!str.includes(".")) {
+            //Use global window object as reference for autocomplete on global
+            this.lastObj = window;
+          }
+        }
+        //If our last object reference is actually an object
+        if (this.lastObj instanceof Object) {
+          //Get its keys
+          this.lastKeys = getProps(this.lastObj);//Object.keys(this.lastObj);
+          //Clear our returned strings
+          this.completeStrings.length = 0;
+          //Loop through keys to see if we can match them with our search prop
+          for (let key of this.lastKeys) {
+            //if (key.startsWith(this.prop)) {
+            if (key.includes(this.prop)) {
+              key = keyToDesc(this.lastObj, key);
+              this.completeStrings.push(key);
+            }
+          }
         }
       }
+      //}
     }
   }
 }
 
-globalThis.onTabComplete = onTabComplete;
+let tabCompleter = new EvalComplete();
+
+globalThis.onTabComplete = function (str) {
+  tabCompleter.complete(str);
+  return tabCompleter.completeStrings;
+}
